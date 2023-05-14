b6ecbd678665aec45016765492408f965b0ed243
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-05-14 11:58:32 +0700

    Create AppController.java

---
+++ ThymleafSpringBoot/src/main/java/com/example/thymleafspringboot/controller/AppController.java
@@ -0,0 +1,11 @@
+package com.example.thymleafspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AppController {
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
+    }
+}
