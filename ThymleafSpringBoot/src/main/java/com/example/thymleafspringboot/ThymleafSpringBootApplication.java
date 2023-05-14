0732db401a9ea898a1be6e386a5cc7454b86b227
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-05-14 11:58:14 +0700

    Create ThymleafSpringBootApplication.java

---
+++ ThymleafSpringBoot/src/main/java/com/example/thymleafspringboot/ThymleafSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymleafspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymleafSpringBootApplication.class, args);
+    }
+
+}
